9350da2e828d39789603a6228831f0d3e3bd32e0
michael-fighting <ding_meng@126.com>
2021-05-12 12:53:16 +0800

    第一次上传远程仓库的文件

---
+++ github.com/Unknown/call/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Unknown/gotest"
+
+func main() {
+	gotest.Hello()
+}
---
+++ github.com/Unknown/gotest/main.go
@@ -0,0 +1,13 @@
+package gotest
+
+import "fmt"
+
+func  Hello()  {
+	fmt.Println("Hello from gotest.")
+	hi()
+
+}
+
+func hi() {
+	fmt.Println("hi from go test.")
+}
