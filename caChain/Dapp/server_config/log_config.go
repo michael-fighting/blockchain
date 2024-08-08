7eb5cf4ee802b6ba131ef07619be3b435d328f6e
michael-fighting <ding_meng@126.com>
2024-08-08 17:27:54 +0800

    reset

--- caChain/Dapp/server_config/log_config.go
+++
@@ -1,8 +0,0 @@
-package serverconfig
-
-type LogConfig struct {
-	LogInConsole bool   `mapstructure:"log_in_console"`
-	ShowColor    bool   `mapstructure:"show_color"`
-	LogLevel     string `mapstructure:"log_level"`
-	LogPath      string `mapstructure:"log_path"`
-}
--- github.com/Unknown/call/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/Unknown/gotest"
-
-func main() {
-	gotest.Hello()
-}
